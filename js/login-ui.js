80a0450127e3396a6feacf30764273ae85d300d1
Johndel B. Bernardo <119475164+Johndel424@users.noreply.github.com>
2026-08-19 16:55:45 +0800

    Add files via upload

--- js/login-ui.js
+++ js/login-ui.js
@@ -182,14 +182,14 @@ loginForm.addEventListener('submit', async (e) => {
   }
 });
 
-// // Security: Disable Context Menu & Developer Shortcuts
-// document.addEventListener('contextmenu', e => e.preventDefault());
-// document.addEventListener('keydown', (e) => {
-//   if (
-//     e.keyCode === 123 || 
-//     (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74 || e.keyCode === 67)) || 
-//     (e.ctrlKey && (e.keyCode === 85 || e.keyCode === 83))
-//   ) {
-//     e.preventDefault();
-//   }
-// });
+// Security: Disable Context Menu & Developer Shortcuts
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('keydown', (e) => {
+  if (
+    e.keyCode === 123 || 
+    (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74 || e.keyCode === 67)) || 
+    (e.ctrlKey && (e.keyCode === 85 || e.keyCode === 83))
+  ) {
+    e.preventDefault();
+  }
+});
